05d7f9a25a3fdc4a6236ab763efc2aeafe4cd6c4
btracey <bdtinternet@gmail.com>
2014-02-17 19:31:33 -0800

    Improved main package documentation

--- async_optimize/doc.go
+++ async_optimize/doc.go
@@ -1,7 +1,10 @@
 // This can be used as a partial introduction to go motivated by the case of
-// function optimization. The code is well commented, but it intended as an aid
-// to teaching go; it is not intended as a text-based tutorial
+// function optimization. The code is well commented, but it intended as a
+// teaching aid, not a text-based tutorial. IF you are reading this from godoc,
+// please see the comments in the source files.
 //
 // The order is as follows
 // 		First
+//		Batch
+//		Async
 package async_optimize
